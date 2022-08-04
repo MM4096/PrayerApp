c72bcdb5a1844bd4253b78f389f4cc3ff06f7fe8
enoch <85976396+MM4096@users.noreply.github.com>
2022-08-04 18:14:39 +1200

    designing main screen - save to work at school

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
-def main():
-    print("Hello world!")
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
 
 
-if __name__ == "__main__":
-    main()
+# Python naming conventions: Capital in front of every word
+# Creates window manager
+class WindowManager(ScreenManager):
+    pass
+
+# screens
+class MainPage(Screen):
+    pass
+
+
+class SecondPage(Screen):
+    pass
+
+
+# loads kv file
+kv = Builder.load_file('main.kv')
+
+
+# app class
+class PrayerApp(App):
+    def build(self):
+        return kv
+
+
+# run app
+if __name__ == '__main__':
+    PrayerApp().run()
