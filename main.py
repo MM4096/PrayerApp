c32c8f36ee086292af9cd7ef7dcabb0475d9b88f
MM4096 <85976396+MM4096@users.noreply.github.com>
2022-08-05 14:37:47 +1200

    Working on main page
    Alpha version

--- main.py
+++ main.py
@@ -2,18 +2,20 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 
+"""Python naming conventions: Capital in front of every word (also because errors with screen manager and camel case 
+naming) Creates window manager"""
 
-# Python naming conventions: Capital in front of every word
-# Creates window manager
+
 class WindowManager(ScreenManager):
     pass
+
 
 # screens
 class MainPage(Screen):
     pass
 
 
-class SecondPage(Screen):
+class MyPrayers(Screen):
     pass
 
 
