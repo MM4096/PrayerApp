7da0ecb21acaf5dec9fafb6578845c8f799e86fa
enoch <85976396+MM4096@users.noreply.github.com>
2022-08-07 17:24:41 +1200

    BUGS EVERYWHERE

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
+import os.path
+
 from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.label import Label
 from kivy.lang import Builder
+from kivy.properties import ObjectProperty
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
+from os.path import exists
+
+from kivy.uix.widget import Widget
 
 """Python naming conventions: Capital in front of every word (also because errors with screen manager and camel case 
 naming) Creates window manager"""
 
-
-class WindowManager(ScreenManager):
-    pass
+# loads kv file
+kv = Builder.load_file('main.kv')
 
 
 # screens
@@ -16,11 +24,26 @@ class MainPage(Screen):
 
 
 class MyPrayers(Screen):
-    pass
+    # code to see how many buttons it should generate
 
+    # checking if file doesn't exist, creates it
+    if not exists("data/LocalPrayers.txt"):
+        if not exists("data/"):
+            os.makedirs("data/")
+        with open("data/LocalPrayers.txt", "x") as f:
+            pass
 
-# loads kv file
-kv = Builder.load_file('main.kv')
+    # reads how many lines are in the file
+    count = 0
+    with open("data/LocalPrayers.txt", "r") as data:
+        for count, line in enumerate(data):
+            pass
+
+    def __init__(self, **kwargs):
+        super(MyPrayers, self).__init__(**kwargs)
+        label = Button(text="hello world!")
+        for i in range(self.count + 1):
+            self.ids.FillIn.add_widget(label)
 
 
 # app class
