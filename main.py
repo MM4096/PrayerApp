e85a37e03a306fb95546d779a643a728e705addd
enoch <85976396+MM4096@users.noreply.github.com>
2022-08-10 19:32:29 +1200

    Fix error: id being called before kv is loaded

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from os.path import exists
 from kivy.config import Config
 
 from kivy.uix.widget import Widget
+
 Config.set("input", 'mouse', "mouse,multitouch_on_demand")
 
 """Python naming conventions: Capital in front of every word (also because errors with screen manager and camel case 
@@ -21,38 +22,28 @@ naming) Creates window manager"""
 class MainPage(Screen):
     pass
 
-
 class MyPrayers(Screen):
-    # code to see how many buttons it should generate
-
-    # checking if file doesn't exist, creates it
-    if not exists("data/LocalPrayers.txt"):
-        if not exists("data/"):
-            os.makedirs("data/")
-        with open("data/LocalPrayers.txt", "x") as f:
-            pass
-
-    # reads how many lines are in the file
-    count = 0
-    with open("data/LocalPrayers.txt", "r") as data:
-        for count, line in enumerate(data):
-            pass
 
     def __init__(self, **kwargs):
         super(MyPrayers, self).__init__(**kwargs)
-        Clock.schedule_once(self.AddButtons)
+        localPrayers = []
+        with open("data/LocalPrayers.txt", "r") as data:
+            for i in range(6):
+                localPrayers.append(data.readline())
+        print(localPrayers)
 
-    def AddButtons(self, *args):
-        box = BoxLayout(orientation="vertical")
-        for i in range(self.count + 1):
-            box.add_widget(Button(text="hello world",
-                                  color=(0, 0, 0),
-                                  background_normal="",
-                                  background_color=(1, 1, 1),
-                                  size_hint=(0.8, 1),
-                                  pos_hint={"x": 0.1}))
-            box.add_widget(Label(size_hint_y=0.2))
-        self.ids.FillIn.add_widget(box)
+        Clock.schedule_once(self.AddButtons(ButtonText=localPrayers, start=0))
+
+    def AddButtons(self, *args, ButtonText, start):
+        for i in range(6):
+            print(i)
+            self.ids.FillIn.add_widget.add_widget(Button(text=ButtonText[i],
+                                                         color=(0, 0, 0),
+                                                         background_normal="",
+                                                         background_color=(1, 1, 1),
+                                                         size_hint=(0.8, 1),
+                                                         pos_hint={"x": 0.1}))
+            self.ids.FillIn.add_widget(Label(size_hint_y=0.2))
 
 
 # app class
@@ -65,4 +56,12 @@ class PrayerApp(App):
 
 # run app
 if __name__ == '__main__':
+    # code to see how many buttons it should generate
+
+    # checking if file doesn't exist, creates it
+    if not exists("data/LocalPrayers.txt"):
+        if not exists("data/"):
+            os.makedirs("data/")
+        with open("data/LocalPrayers.txt", "x") as f:
+            pass
     PrayerApp().run()
