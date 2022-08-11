816264f3c6aa8e4e9d1c3e16c0a4791050197d12
enoch <85976396+MM4096@users.noreply.github.com>
2022-08-11 19:20:49 +1200

    Added dynamic buttons

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
 import os.path
-from kivy.clock import Clock
-from kivy.app import App
-from kivy.graphics import Color
-from kivy.metrics import dp
-from kivy.uix.button import Button
-from kivy.uix.label import Label
-from kivy.lang import Builder
-from kivy.properties import ObjectProperty, ListProperty
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.screenmanager import ScreenManager, Screen
 from os.path import exists
+
+from kivy.app import App
+from kivy.clock import Clock
 from kivy.config import Config
 from kivy.graphics import *
-
-from kivy.uix.widget import Widget
+from kivy.lang import Builder
+from kivy.metrics import dp
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.screenmanager import Screen
 
 Config.set("input", 'mouse', "mouse,multitouch_on_demand")
 
@@ -30,45 +27,58 @@ class MyPrayers(Screen):
 
     def __init__(self, **kwargs):
         super(MyPrayers, self).__init__(**kwargs)
+        # creating the variables here first
         self.PrayerBox = BoxLayout()
         self.rectangle = None
+        # draws layout
         Clock.schedule_once(self.DrawLayout)
+        # Reads 6 lines at a time, to get your prayer
         localPrayers = []
         with open("data/LocalPrayers.txt", "r") as data:
             for i in range(6):
                 localPrayers.append(data.readline())
-        print(localPrayers)
-        Clock.schedule_once(self.AddButtons(ButtonText=localPrayers))
+        # removes \n or \r\n from the end of each line
+        localPrayers = [x.rstrip() for x in localPrayers]
+        # add the buttons: lambda dt removes the callback
+        Clock.schedule_once(lambda dt: self.AddButtons(localPrayers))
 
     def DrawLayout(self, *args):
+        # BoxLayout
         MainBox = BoxLayout(orientation="vertical")
+        # Adds the title
         TitleLabel = Label(text="Personal Prayer List",
                            size_hint_y=0.3,
                            font_size=dp(32),
                            color=(1, 1, 1))
+        # BoxLayout for the prayers
         self.PrayerBox = BoxLayout(orientation="vertical")
 
+        # background
         with self.canvas:
             Color(rgb=(0.25, 0.45, 0.62))
-
+            # dynamic rectangle
             self.rectangle = Rectangle(pos=self.center, size=(self.width / 2, self.height / 2))
             self.bind(pos=self.UpdateRectangle, size=self.UpdateRectangle)
+        # adding widgets
         MainBox.add_widget(TitleLabel)
         MainBox.add_widget(self.PrayerBox)
         self.add_widget(MainBox)
 
     def UpdateRectangle(self, *args):
+        # updates background rectangle
         self.rectangle.pos = self.pos
         self.rectangle.size = self.size
 
-    def AddButtons(self, *args, ButtonText):
+    def AddButtons(self, *ButtonText):
         for i in range(6):
-            self.PrayerBox.add_widget(Button(text=ButtonText[i],
+            # Creates the styled buttons
+            self.PrayerBox.add_widget(Button(text=str(ButtonText[0][i]),
                                              color=(0, 0, 0),
                                              background_normal="",
                                              background_color=(1, 1, 1),
                                              size_hint=(0.8, 1),
                                              pos_hint={"x": 0.1}))
+            # margin between buttons
             self.PrayerBox.add_widget(Label(size_hint_y=0.2))
 
 
